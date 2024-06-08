3bb76b5d3dcb92592a65d9c1e3e3cc3d2cac96ce
IanFan <ianfan0704@gmail.com>
2024-06-08 10:44:07 +0800

    App: build project, support iOS16, portrait

---
+++ Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank
+//
+//  Created by Ian Fan on 2024/6/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
