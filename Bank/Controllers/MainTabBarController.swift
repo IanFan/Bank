e52f86b9bbf461fcc2f8e4b736c52bde1be08d33
IanFan <ianfan0704@gmail.com>
2024-06-08 14:10:40 +0800

    Refactor: TabBar

--- Bank/Controllers/MainTabBarController.swift
+++ Bank/Controllers/MainTabBarController.swift
@@ -55,11 +55,13 @@ enum MainTabEnum: Int {
 }
 
 class MainTabBarController: UITabBarController {
+    var customTabBar: MainTabBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBar()
     }
-
+    
     private func setupTabBar() {
         tabBar.isHidden = true
         
@@ -69,19 +71,9 @@ class MainTabBarController: UITabBarController {
         let forthVC = createMainViewController(tabEnum: .Service)
         self.viewControllers = [firstVC, secondVC, thirdVC, forthVC]
         
-        let customTabBar = createCustomTabBar()
-        adjustTabBarPostion(tabBar: customTabBar)
-        roundTabBar(tabBar: customTabBar)
-    }
-    
-    private func createCustomTabBar() -> UITabBar {
-        let customTabBar = UITabBar()
-        customTabBar.items = tabBar.items
-        customTabBar.barTintColor = .clear
-        customTabBar.backgroundImage = UIImage()
-        customTabBar.shadowImage = UIImage()
+        let customTabBar = MainTabBar()
         self.view.addSubview(customTabBar)
-        return customTabBar
+        customTabBar.setupTabBar(tabBarConroller: self)
     }
     
     private func createMainViewController(tabEnum: MainTabEnum) -> HomeViewController {
@@ -99,8 +91,8 @@ class MainTabBarController: UITabBarController {
         
         let title = isPad() ? "" : titleStr
         let tab = UITabBarItem.init(title: title,
-                                         image: UIImage(named: imageName)?.withRenderingMode(.alwaysOriginal),
-                                         selectedImage: UIImage(named: selectedImageName)?.withRenderingMode(.alwaysOriginal))
+                                    image: UIImage(named: imageName)?.withRenderingMode(.alwaysOriginal),
+                                    selectedImage: UIImage(named: selectedImageName)?.withRenderingMode(.alwaysOriginal))
         tab.setTitleTextAttributes([.foregroundColor : UIColor(hexString: tabTitleColorHex)], for: .normal)
         tab.setTitleTextAttributes([.foregroundColor : UIColor(hexString: tabTitleColorHexSelect)], for: .selected)
         tab.tag = tabEnum.rawValue
@@ -108,40 +100,5 @@ class MainTabBarController: UITabBarController {
         tab.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 10)
         tab.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
         return tab
-    }
-    
-    private func roundTabBar(tabBar: UITabBar) {
-        let layer = CAShapeLayer()
-        layer.path = UIBezierPath(roundedRect: CGRect(x: 30, y: self.tabBar.bounds.minY + 5, width: self.tabBar.bounds.width - 60, height: self.tabBar.bounds.height + 10), cornerRadius: (self.tabBar.frame.width/2)).cgPath
-        layer.shadowColor = UIColor.lightGray.cgColor
-        layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
-        layer.shadowRadius = 5.0
-        layer.shadowOpacity = 0.3
-        layer.borderWidth = 1.0
-        layer.opacity = 1.0
-        layer.isHidden = false
-        layer.masksToBounds = false
-        layer.fillColor = UIColor.white.cgColor
-  
-        tabBar.layer.insertSublayer(layer, at: 0)
-    }
-    
-    private func adjustTabBarPostion(tabBar: UITabBar) {
-        tabBar.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            tabBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            tabBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            tabBar.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -70),
-            tabBar.heightAnchor.constraint(equalToConstant: 49)
-        ])
-        
-        tabBar.itemPositioning = .centered // 可以选择 .automatic 或 .fill 来调整布局方式
-        tabBar.itemSpacing = 5
-    }
-}
-
-extension MainTabBarController: MainTabBarDelegate {
-    func tabBar(_ tabBar: MainTabBar, didSelectTabAt index: Int) {
-        selectedIndex = index
     }
 }
--- Bank/Views/MainTabBar.swift
+++ Bank/Views/MainTabBar.swift
@@ -8,44 +8,56 @@
 import Foundation
 import UIKit
 
-protocol MainTabBarDelegate: AnyObject {
-    func tabBar(_ tabBar: MainTabBar, didSelectTabAt index: Int)
-}
-
-class MainTabBar: UIView {
-    weak var delegate: MainTabBarDelegate?
-
+class MainTabBar: UITabBar {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    private func setupView() {
-        backgroundColor = .randomColor
-        layer.shadowOffset = CGSize(width: 0, height: -3)
-        layer.shadowOpacity = 0.3
-        layer.shadowRadius = 6
-
-        let numberOfTabs = 3
-        let buttonWidth = frame.width / CGFloat(numberOfTabs)
-        let buttonHeight = frame.height
-
-        for i in 0..<numberOfTabs {
-            let button = UIButton(type: .custom)
-            button.frame = CGRect(x: CGFloat(i) * buttonWidth, y: 0, width: buttonWidth, height: buttonHeight)
-            button.tag = i
-            button.setImage(UIImage(named: "icTabbarHomeActive"), for: .normal)
-            button.imageEdgeInsets = UIEdgeInsets(top: -10, left: 0, bottom: 0, right: 0) // Adjust icon position
-            button.addTarget(self, action: #selector(tabButtonTapped(_:)), for: .touchUpInside)
-            addSubview(button)
-        }
+    
+    func setupTabBar(tabBarConroller: UITabBarController) {
+        setup(tabBarConroller: tabBarConroller)
+        roundTabBar(tabBarConroller: tabBarConroller)
+        adjustTabBarPostion(tabBarConroller: tabBarConroller)
+    }
+    
+    private func setup(tabBarConroller: UITabBarController) {
+        items = tabBarConroller.tabBar.items
+        barTintColor = .clear
+        backgroundImage = UIImage()
+        shadowImage = UIImage()
     }
 
-    @objc private func tabButtonTapped(_ sender: UIButton) {
-        delegate?.tabBar(self, didSelectTabAt: sender.tag)
+    private func roundTabBar(tabBarConroller: UITabBarController) {
+        let tabBar = self
+        let layer = CAShapeLayer()
+        layer.path = UIBezierPath(roundedRect: CGRect(x: 30, y: tabBarConroller.tabBar.bounds.minY + 5, width: tabBarConroller.tabBar.bounds.width - 60, height: tabBarConroller.tabBar.bounds.height + 10), cornerRadius: (tabBarConroller.tabBar.frame.width/2)).cgPath
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        layer.shadowRadius = 5.0
+        layer.shadowOpacity = 0.3
+        layer.borderWidth = 1.0
+        layer.opacity = 1.0
+        layer.isHidden = false
+        layer.masksToBounds = false
+        layer.fillColor = UIColor.white.cgColor
+  
+        tabBar.layer.insertSublayer(layer, at: 0)
+    }
+    
+    private func adjustTabBarPostion(tabBarConroller: UITabBarController) {
+        let tabBar = self
+        tabBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tabBar.leadingAnchor.constraint(equalTo: tabBarConroller.view.leadingAnchor),
+            tabBar.trailingAnchor.constraint(equalTo: tabBarConroller.view.trailingAnchor),
+            tabBar.bottomAnchor.constraint(equalTo: tabBarConroller.view.bottomAnchor, constant: -70),
+            tabBar.heightAnchor.constraint(equalToConstant: 49)
+        ])
+        
+        tabBar.itemPositioning = .centered
+        tabBar.itemSpacing = 5
     }
 }
